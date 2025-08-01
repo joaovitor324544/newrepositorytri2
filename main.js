294b08247cce3fe9875bc86c4ef673c3dae46e9d
joaovitor324544 <140770599+joaovitor324544@users.noreply.github.com>
2025-08-01 13:46:52 +0000

    save01]08

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const numeroSenha = document.querySelector('.parametro-senha__textos');
+const numeroSenha = document.querySelector('.parametro-senha__texto');
 let tamanhoSenha = 12;
 numeroSenha.textContent = tamanhoSenha;
  
