28679bcf43a77e988ddbce32317ad300c61d5c84
joaovitor324544 <140770599+joaovitor324544@users.noreply.github.com>
2025-08-01 13:41:49 +0000

    save

---
+++ main.js
@@ -0,0 +1,21 @@
+const numeroSenha = document.querySelector('.parametro-senha-texto');
+let tamanhoSenha = 12;
+numeroSenha.textContent = tamanhoSenha;
+ 
+const botoes = document.querySelector('.parametro-senha__botoes');
+
+botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho; 
+ 
+function diminuiTamanho(){
+    if(tamanhoSenha > 1){
+        tamanhoSenha--;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+}
+function diminuiTamanho(){
+    if(tamanhoSenha < 20){
+        tamanhoSenha++;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+}
