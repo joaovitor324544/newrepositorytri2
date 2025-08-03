319dd3e2402a583f5a55024604665ed3ee027e00
joaovitor324544 <140770599+joaovitor324544@users.noreply.github.com>
2025-08-03 18:24:49 +0000

    save03/08 correcoes com gpt

--- main.js
+++ main.js
@@ -1,21 +1,29 @@
 const numeroSenha = document.querySelector('.parametro-senha__texto');
 let tamanhoSenha = 12;
 numeroSenha.textContent = tamanhoSenha;
- 
-const botoes = document.querySelector('.parametro-senha__botoes');
 
-botoes[0].onclick = diminuiTamanho;
-botoes[1].onclick = aumentaTamanho; 
- 
-function diminuiTamanho(){
-    if(tamanhoSenha > 1){
+
+const botaoMenos = document.getElementById('botao_menos');
+const botaoMais = document.getElementById('botao_mais');
+
+
+botaoMenos.addEventListener('click', diminuiTamanho);
+botaoMais.addEventListener('click', aumentaTamanho);
+
+
+function diminuiTamanho() {
+    if (tamanhoSenha > 1) {
         tamanhoSenha--;
+        numeroSenha.textContent = tamanhoSenha;
     }
-    numeroSenha.textContent = tamanhoSenha;
+      numeroSenha.textContent = tamanhoSenha;
 }
-function diminuiTamanho(){
-    if(tamanhoSenha < 20){
+
+
+function aumentaTamanho() {
+    if (tamanhoSenha < 20) {
         tamanhoSenha++;
+        numeroSenha.textContent = tamanhoSenha;
     }
     numeroSenha.textContent = tamanhoSenha;
 }
